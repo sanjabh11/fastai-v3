96a10ab5a9812c46fff50956162fb6feb62b0152
sanjabh11 <33122397+sanjabh11@users.noreply.github.com>
2019-03-30 15:07:36 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1-F9c-0cxkmw2rZPGTMiBNPtd0Hyqhovi'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Cubic_Zirconia_diamond', 'Zircon_diamond', 'Moissanite_diamond', 'White_Sapphire_diamond', 'Rutile_diamond', 'Spinel_diamond', 'Synthetic_Garnet_diamond', 'Type_Ia_diamond', 'Type_IIa_diamond', 'type_Ib_diamond', 'type_IIb_diamond']
+path = Path(__file__).parent
 path = Path(__file__).parent
 
 app = Starlette()
